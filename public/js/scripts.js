e9a09c8fed552cd43f835a46768cbe84a57672cc
Cem Ozturan <cozturan@findmypast.com>
2016-09-02 17:18:39 +0100

    59px is fine, 60px causes a bug where the page thinks the gallery is active when we click on features

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -10,7 +10,7 @@
     }), (new WOW).init(), t("a.page-scroll").bind("click", function(a) {
         var e = t(this);
         t("html, body").stop().animate({
-            scrollTop: t(e.attr("href")).offset().top - 50 // 50 is how much space is left at the top of the screen when a section is navigated into
+            scrollTop: t(e.attr("href")).offset().top - 59 // 59 is how much space is left at the top of the screen when a section is navigated into
         }, 1450, "easeInOutExpo"), a.preventDefault() // 1450 is the ms for the scrolling animation
     }), t(".navbar-collapse ul li a").click(function() {
         t(".navbar-toggle:visible").click()
