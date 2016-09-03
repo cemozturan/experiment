8e9ee7da9c405ec5e2da03ee0bd9cee5b3c9e845
Cem <cemozturan@gmail.com>
2016-09-03 13:21:13 +0100

    changed alternative text for images

--- src/gallery/photo-directive.js
+++ src/gallery/photo-directive.js
@@ -6,6 +6,7 @@
     .directive('photoDirective', [photoDirective]);
 
   function photoDirective() {
+
     var controller = ['$scope', function($scope){
       $scope.url = './public/media/' + $scope.id + '.jpg';
     }];
@@ -13,7 +14,7 @@
     var template =
               '<div class="col-lg-4 col-sm-6">' +
                 '<a href="#galleryModal" class="gallery-box" data-toggle="modal" data-src="{{url}}">' +
-                '<img ng-src="{{url}}" class="img-responsive" alt="Image 1">' +
+                '<img ng-src="{{url}}" class="img-responsive" alt="{{id}}">' +
                 '<div class="gallery-box-caption">' +
                   '<div class="gallery-box-content">' +
                     '<div>' +
