3f78eb0cb1bb3f3b211892114c79fabcbd78a841
Cem <cemozturan@gmail.com>
2016-09-03 15:58:36 +0100

    pulling alert modal html into a component directive

---
+++ src/modals/alert-modal-directive.js
@@ -0,0 +1,27 @@
+(function(){
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('alertModalDirective', [alertModalDirective]);
+
+    function alertModalDirective() {
+
+            return {
+                template:
+                '<div id="alertModal" class="modal fade" tabindex="-1" role="dialog" aria-hidden="true">' +
+                    '<div class="modal-dialog modal-sm">' +
+                        '<div class="modal-content">' +
+                    	   '<div class="modal-body">' +
+                                '<h2 class="text-center">Nice Job!</h2>' +
+                    		    '<p class="text-center">You clicked the button, but it does not actually go anywhere because this is only a demo.</p>' +
+                    		     '<p class="text-center"><a href="http://www.bootstrapzero.com">Learn more at BootstrapZero</a></p>' +
+                    		    '<br/>' +
+                    		    '<button class="btn btn-primary btn-lg center-block" data-dismiss="modal" aria-hidden="true">OK <i class="ion-android-close"></i></button>' +
+                    	    '</div>' +
+                        '</div>' +
+                    '</div>' +
+                '</div>'
+            }
+    };
+})();
