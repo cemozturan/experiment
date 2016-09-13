ac522e6eec67b92ae897899ac5583b80317a000b
Cem Ozturan <cozturan@findmypast.com>
2016-09-13 18:28:50 +0100

    tiny bit of styling for the game adding lots of voice messages and conditions for each

--- src/mastermind/gameCtrl.js
+++ src/mastermind/gameCtrl.js
@@ -3,10 +3,13 @@
 
   var controllerId = 'GameController';
 
-      angular
+  angular
     .module('app')
     .controller('GameController', ['loggerService', GameController]);
 
+    /*
+      Has a lot of code that should live in services, not structured at all...
+    */
     function GameController(loggerService) {
         var ctrl = this;
 
@@ -15,22 +18,19 @@
         ctrl.opponentGuessData = [];
         ctrl.processGuess = function(guess){
             if (!isNumberValid(guess)){
+                sayRandomMessage(invalidInputMessages);
                 ctrl.invalidNumberEntered = true;
                 return;
             }
             ctrl.invalidNumberEntered = false;
             var result = getGuessEvaluation(ctrl.myNumber, guess);
-            if (result === "+5"){
-                responsiveVoice.speak("Elinize verildi, canınız sağolsun. Ağlamak yok, tamam mı? Ağlamak varsa bilelim", "Turkish Female");
-            } else {
-                responsiveVoice.speak(result.toString(), "Turkish Female");
-            }
             ctrl.opponentGuessData.push({guess: guess, result: result});
             ctrl.clearOpponentGuess();
         };
 
         ctrl.startGame = function(myNumber){
             if (!isNumberValid(myNumber)){
+                sayRandomMessage(invalidInputMessages);
                 ctrl.invalidNumberEntered = true;
                 return;
             }
@@ -38,6 +38,7 @@
             ctrl.myNumber = myNumber;
             ctrl.myNumberStr = myNumber.toString();
             ctrl.gameStarted = true;
+            sayRandomMessage(gameStartMessages);
         };
 
         function getGuessEvaluation (value, guess){
@@ -63,6 +64,27 @@
             } else if (wrongLocation > 0) {
                 result = '-' + wrongLocation;
             }
+            if (result === "+5") {
+              sayRandomMessage(correct5_messages);
+            } else if (result === "+4") {
+              sayRandomMessage(correct_plus_4messages);
+            } else {
+              if (correctLocation + wrongLocation === 5 && result !== "+5") {
+                var message;
+                if (correctLocation > 2) {
+                  message = result.toString() + getRandomMessage(correct3_4messages);
+                  responsiveVoice.speak(message, "Turkish Female");
+                } else {
+                  message = result.toString() + getRandomMessage(correct1_2messages);
+                  responsiveVoice.speak(message, "Turkish Female");
+                }
+              } else if (result === "0") {
+                  sayRandomMessage(zeroMessages);
+              } else {
+                responsiveVoice.speak(result.toString(), "Turkish Female");
+              }
+            }
+
             return result;
         }
 
@@ -81,5 +103,69 @@
           ctrl.opponentGuess = "";
           ctrl.invalidNumberEntered = false;
         };
+
+        function sayRandomMessage(arr) {
+          var message = getRandomMessage(arr);
+          responsiveVoice.speak(message, "Turkish Female");
+        }
+
+        function getRandomMessage(arr) {
+            var total = arr.length;
+            return arr[getRandomInt(0, total)];
+        }
+
+        function getRandomInt(min, max) {
+          min = Math.ceil(min);
+          max = Math.floor(max);
+          return Math.floor(Math.random() * (max - min)) + min;
+        }
+
+        var gameStartMessages = [
+          "Oyun başladı. Sevgiliniz Cemoş size iyi şanslar diler.",
+          "İyi olan kazansın, bol şans.",
+          "Meşhur sayı oyunu başlamıştır, iyi şanslar.",
+          "Aklınıza kuvvet, haydi göreyim sizi.",
+          "Haydi şapşikim benim, ele vermek senin göbek adın.",
+          "Oyun başladı. İçimden bir ses kazanacaksınız diyor."
+        ];
+        var correct1_2messages = [
+          " . Ha siktir, neyse, panik olmayın. Çıkmadık candan umut kesilmez."
+        ];
+        var correct3_4messages = [
+          " . Elinize verilmek üzere, lütfen avcunuzu açınız.",
+          " . Cemoş var ya Cemoş, yer sizi yaa, ama maalesef elinize verilmek üzere."
+        ];
+        var correct_plus_4messages = [
+          " +4 . Başınız dertte gibi, bir an önce aradığınız sayıyı bulmanız gerekiyor.",
+          " +4 . Zannedersem bu sıçtığınızın resmidir."
+        ];
+        var correct5_messages = [
+          "Elinize verildi, canınız sağolsun. Cemoş sizi çok seviyor, o yüzden ağlamak yok, tamam mı? Gene de ağlamak varsa bilelim",
+          "Evet, yenildiniz. Ama rakibiniz sizi sevdiği için yenildiniz.",
+          "Size yine hüsran, sevdiğinize yine bayram var, geçmiş olsun.",
+          "Bilgisayarınızı koltuğunuzun altına alınız ve gülümseyiniz, yenildiniz.",
+          "Geçmiş olsun, sevgiliniz size Ağlama Değmez Hayat Bu Gözyaşlarına şarkısını armağan ediyor.",
+          "Kaderde varsa yenilmek, neye yarar üzülmek? O yüzden, üzülmeyiniz.",
+          "Maalesef yenildiniz. Bir dost tavsiyesi, yenilginin acısına en iyi gelen şey patates pilav yapmaktır.",
+          "üzgünüm şapşikliğiniz yenilmenize engel olamadı.",
+          "Allahım ya rabbim, böyle bir şapşiklik olamaz. Ama yenildiniz.",
+        ];
+        var zeroMessages = [
+          "Ooooof, 0, rakibiniz şahane bir tahmin yaptı, galiba sıçtınız.",
+          "Aman tanrım, 0.",
+          "Allahım, olamaz böyle bir şey, 0.",
+          "Ooy ooy oy, 0, sıkıntı büyük",
+          "Hahaha, 0, hihohaha",
+          "Vay anasını, 0, rakibinizdeki şans sizde olsa çoktan kazanmıştınız.",
+          "0 0 0, inanamıyorum yaa."
+        ];
+        var invalidInputMessages = [
+          "Hatalı sayı girdiniz. Lütfen daha dikkatli olunuz. Cemoş'u öpünüz.",
+          "Maalesef şapşikliğiniz hatalı sayı girmenize engel olamıyor.",
+          "Gene hatalı sayı girdiniz, rica ederim dikkatli olunuz.",
+          "Kurallar açık ve net, lütfen kurallara uygun bir sayı giriniz.",
+          "Neyiniz var kuzum? Bu sayı hatalı, görmüyor musunuz?",
+          "Cemoş var ya Cemoş, yer sizi yaa, ama bu sayı hatalı.",
+        ];
     }
 })();
